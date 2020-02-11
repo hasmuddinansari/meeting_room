f02804d3759cb1233add788e3c5afc803b6aba06
Hasmuddin <mdaliansari33@gmail.com>
2020-02-11 20:31:16 +0530

    modify history

--- src/components/common/Book/BookedHistory.js
+++ src/components/common/Book/BookedHistory.js
@@ -4,33 +4,36 @@ import React from 'react'
 function BookedHistory(props) {
     const booked = JSON.parse(localStorage.getItem("orders"))
     console.log("booked", booked)
-    if(booked.length!==0 || booked!==null){
+    if(booked.length==0 || booked==null){
     return (
-        <table className="table">
-            <thead className="bg-light">
-                <tr>
-                    <td>Meeting Room</td>
-                    <td>Price</td>
-                    <td>Floor</td>
-                    <td>From</td>
-                    <td>To</td>
-                </tr>
-            </thead>
-            <tbody>
-                {booked.map(room=>{
-                    return <tr>
-                        <td>{room.room_name}</td>
-                        <td>{room.price}</td>
-                        <td>{room.floor_num}</td>
-                        <td>{room.from}</td>
-                        <td>{room.to}</td>
-                    </tr>
-                })}
-            </tbody>
-        </table>
-    )}
+        <h4>No meeting Rooms are booked</h4>
+    )
+    }
     else{
-        return <h2>No booking History Available</h2>
+        return (
+            <table className="table">
+        <thead className="bg-light">
+            <tr>
+                <td>Meeting Room</td>
+                <td>Price</td>
+                <td>Floor</td>
+                <td>From</td>
+                <td>To</td>
+            </tr>
+        </thead>
+        <tbody>
+            {booked.map(room=>{
+                return <tr>
+                    <td>{room.room_name}</td>
+                    <td>{room.price}</td>
+                    <td>{room.floor_num}</td>
+                    <td>{room.from}</td>
+                    <td>{room.to}</td>
+                </tr>
+            })}
+        </tbody>
+    </table>
+        )
     }
 }
 
